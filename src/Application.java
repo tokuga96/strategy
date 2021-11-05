487b27bf15bc35255c91f074ffc762c5b4c368b7
tokuga96 <93782178+tokuga96@users.noreply.github.com>
2021-11-05 16:06:46 +0000

    Fix typo.

--- src/Application.java
+++ src/Application.java
@@ -4,6 +4,6 @@ public class Application {
         mallardDuck.performFly();
         //duck gets hit in the right wing
         mallardDuck.setFlyBehaviour(new FlyWithLeftWing());
-        mallardDuck.performFly(); //tries to fly away the *left* wing
+        mallardDuck.performFly(); //tries to fly away with the *left* wing
     }
 }
